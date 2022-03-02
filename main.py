2a76e5383b37606e3c2a9022678a8e86f6733858
Maaz Omair <63117337+maaz1234@users.noreply.github.com>
2022-03-02 05:48:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+money_machine = MoneyMachine()
+money_machine.report()
+coffee_maker = CoffeeMaker()
+coffee_maker.report()
+menu = Menu()
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice = input(f"what would u like to have!({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            money_machine.make_payment(drink.cost)
+            coffee_maker.make_coffee(drink)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
